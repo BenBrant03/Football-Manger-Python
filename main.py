3ec001e819e7899e9846acda7bb37a80dc7d3a28
BenBrant03 <73722053+BenBrant03@users.noreply.github.com>
2021-03-13 22:41:27 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import random
 from tkinter import *
+import os
+from csv import reader
+#Veriables
 leagues = ["Premier League" , "La Liga" , "BundesLiga"]
 premierLeague = ["Arsenal" , "Aston Villa" , "Brighton" , "Burnely" , "Chelsea", "Crystal Palace", "Everton", "Fulham" , "Leeds" , "Leicester City" , "Liverpool" , "Manchester City" ,"Manchester United" , "Newcastle" , "Shefield United", "Southampton" ,"Tottenham" ,"West Brom" ,"West Ham","Wolves"]
 leagueTable =[]
+fixturesList = []
 playerTeam  = None
 confirm = False
 run = True
+matchday = 1
+leagueCode = None 
 version = "Pre Alpha 1.1"
+playerFixture = None
+
+#Algorithims
 def NewOrLoad():
   global load
   global new
@@ -40,10 +49,13 @@ def setup():
   Enter The Number Below
   """)
   if league == "1":
+    global leagueCode
+    leagueCode = "PL"
     global premierLeague
     with open("league.txt" , "w") as leagueFile:
       leagueFile.write("Premier League")
       leagueFile.close()
+      os.system('clear')
     print("Which Team?")
     for i in range(len(premierLeague)):
       print(str(i) + " " + premierLeague[i])
@@ -79,9 +91,9 @@ def confirmYOrN():
 
 def premierLeagueTableSetup():
   global premierLeague
-  with open("leagueTable.txt", "a") as leagueTableFile:
+  with open("leagueTable.txt", "w") as leagueTableFile:
     for i in range(len(premierLeague)):
-      leagueTable.append([[premierLeague[i]],[0]])
+      leagueTable.append([premierLeague[i],0])
     leagueTableFile.write(str(leagueTable))
     print("League Table Setup Sucessfull")
     leagueTableFile.close()
@@ -98,10 +110,84 @@ def premierLeagueTableSetup():
         location = aw'''''
 #random fixture generation coming Soon
     
+def loadFixtures():
+  global matchday
+  global leagueCode
+  with open (str(leagueCode) + "matchday"+str(matchday)+".txt" , "r") as fixtureFile:
+    global fixturesList
+    for line in fixtureFile:
+      stripedLine = line.strip()
+      lineList = stripedLine.split()
+      fixturesList.append(lineList)   
+    fixtureFile.close()
+  print("Fixtures Loaded")
 
+def matchdayPage():
+  global matchday
+  global fixturesList
+  global playerTeam
+  global leagueTable
+  global playerFixture
+  correctInput = False
+  saveLoction = None
+  otherFixtures = []
+  counted = 0
+  playerFixture = []
+  postion = 1
+  print("Matchday" + str(matchday))
+  print("Your Next Game is:")
+  if len(playerTeam) == 1:
+    for i in fixturesList:
+      for y in i:
+        if y == playerTeam:
+          playerFixture = i
+          print(*i)
+        elif i not in otherFixtures:
+          if i not in playerFixture:
+            otherFixtures.append(i)
+  else:
+    firstWord = playerTeam.split()[0]
+    for i in fixturesList:
+      for y in i:
+        if y == firstWord:
+          playerFixture = i
+          print(*i)
+        elif i not in otherFixtures:
+          if i not in playerFixture:
+            otherFixtures.append(i)
+  print('     ')
+  print('The Other Fixtures')
+  for i in otherFixtures:
+    print(*i)
+  print("Premier League")
+  print("Team          Pts")
+  leagueTable.sort()
+  for i in leagueTable:
+    print(*i)
+  while correctInput == False:
+    inputKey = input('Type a to Sim')
+    if inputKey == 'a':
+      correctInput = True
+    else:
+      print('Please type a and press enter')
+  
 
+def clear():
+  os.system('clear')
 
+def matchEngine():
+  global playerTeam
+  global playerFixture
+  global matchday
+  homeTeam = playerFixture[0]
+  if playerFixture[2] != "v":
+    awayTeam = playerFixture[2]
+  else:
+    awayTeam = playerFixture[3]
+  
 
+#Logic
+clear()
 print("Python Football Sim")
 print("Version - " + version)
 print("""
@@ -109,7 +195,12 @@ print("""
 """)
 #NewOrLoad()
 setup()
+clear()
 if playerTeam in premierLeague:
     premierLeagueTableSetup()
+    loadFixtures()
+    clear()
+    matchdayPage()
+    clear()
 
 
